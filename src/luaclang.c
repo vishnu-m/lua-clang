76c020dcf28de44ba4b0b69a2a96de880d1f42e0
Vishnu-M <vishnumurali7000@gmail.com>
2019-07-11 23:56:52 +0530

    Introduced clang object

--- src/luaclang.c
+++ src/luaclang.c
@@ -5,50 +5,68 @@
 #include "lualib.h"
 #include "lauxlib.h"
 
-#define INDEX_METATABLE  "Clang.Index"
-#define TU_METATABLE     "Clang.TU"
+#define CLANG_METATABLE  "Clang.Object"
 #define CURSOR_METATABLE "Clang.Cursor"
 #define TYPE_METATABLE   "Clang.Type"
 
-/* Create CXIndex as userdata */
+typedef struct clang_object {
+        CXIndex idx;
+        CXTranslationUnit tu;
+} clang_object;
 
-static CXIndex *new_CXIndex(lua_State *L) 
+/* Create clang_object as userdata */
+static clang_object *new_clangobject(lua_State *L) 
 {
-        CXIndex *idx = (CXIndex*) lua_newuserdata(L, sizeof(CXIndex));
-        luaL_getmetatable(L, INDEX_METATABLE);
+        clang_object *clang_parser = (clang_object*) lua_newuserdata(L, sizeof(clang_object));
+        luaL_getmetatable(L, CLANG_METATABLE);
         lua_setmetatable(L, -2);
-        return idx;
+        return clang_parser;
 }
 
-/* Convert userdata type to CXIndex */
-
-static CXIndex to_CXIndex(lua_State *L, int n)
+/* Convert userdata type to  */
+static clang_object to_clangobject(lua_State *L, int n) 
 {
-        CXIndex *idx = (CXIndex*) luaL_checkudata(L, n, INDEX_METATABLE);
-        return *idx;
+        clang_object *clang_parser = (clang_object*) luaL_checkudata(L, n, CLANG_METATABLE);
+        return *clang_parser;
 }
 
-/* Create CXTranslatioUnit as userdata */
-
-static CXTranslationUnit *new_CXTU(lua_State *L)
+/*      
+        Format - newParser(file_name)
+        Parameter - file_name - The name of the source file to load 
+        More info - 1. https://clang.llvm.org/doxygen/group__CINDEX.html#ga51eb9b38c18743bf2d824c6230e61f93
+                    2. https://clang.llvm.org/doxygen/group__CINDEX__TRANSLATION__UNIT.html#ga2baf83f8c3299788234c8bce55e4472e
+        Returns clang object whose translation unit cursor can be obtained.
+*/
+static int create_clangobject(lua_State *L)
 {
-        CXTranslationUnit *tu = (CXTranslationUnit*) lua_newuserdata(L, sizeof(CXTranslationUnit));
-        luaL_getmetatable(L, TU_METATABLE);
-        lua_setmetatable(L, -2);
-        return tu;
+        const char *file_name = lua_tostring(L, 1);
+        clang_object *clang_parser = new_clangobject(L);
+        clang_parser->idx = clang_createIndex(1, 0);
+        const char *args[] = {file_name};
+        clang_parser->tu = clang_parseTranslationUnit(clang_parser->idx, 0, args, 1, 0, 0, CXTranslationUnit_None);
+        return 1;
 }
 
-/* Convert userdata type to CXTranslatioUnit */
-
-static CXTranslationUnit to_CXTU(lua_State *L, int n) 
+/*      
+        Format - parserdisposeParser()
+        Parameter - parser - Clang object to be disposed
+        More info - 1. https://clang.llvm.org/doxygen/group__CINDEX.html#ga51eb9b38c18743bf2d824c6230e61f93
+                    2. https://clang.llvm.org/doxygen/group__CINDEX__TRANSLATION__UNIT.html#ga2baf83f8c3299788234c8bce55e4472e
+        Returns nothing
+*/
+static int dispose_clangobject(lua_State *L)
 {
-        CXTranslationUnit *tu = (CXTranslationUnit*) luaL_checkudata(L, n, TU_METATABLE);
-        return *tu;
+        clang_object clang_parser = to_clangobject(L, 1);
+        if (clang_parser.idx == NULL) return 0;
+        clang_disposeIndex(clang_parser.idx);
+        clang_disposeTranslationUnit(clang_parser.tu);
+        clang_parser.idx = NULL;
+        clang_parser.tu = NULL;
+        return 0;
 }
 
 /* Create CXCursor as userdata */
-
-static CXCursor *new_CXCursor(lua_State *L) 
+static inline CXCursor *new_CXCursor(lua_State *L) 
 {
         CXCursor *cur = (CXCursor*) lua_newuserdata(L, sizeof(CXCursor));
         luaL_getmetatable(L, CURSOR_METATABLE);
@@ -57,12 +75,37 @@ static CXCursor *new_CXCursor(lua_State *L)
 }
 
 /* Convert userdata type to CXCursor */
-
-static CXCursor to_CXCursor(lua_State *L, int n) 
+static  CXCursor to_CXCursor(lua_State *L, int n) 
 {
         CXCursor *c = (CXCursor*) luaL_checkudata(L, n, CURSOR_METATABLE);
         return *c;
 }
+
+/*      
+        Format - parser:getCursor()
+        Parameter - parser - Clang object whose translation unit cursor is to be obtained 
+        More info - https://clang.llvm.org/doxygen/group__CINDEX__CURSOR__MANIP.html#gaec6e69127920785e74e4a517423f4391
+        Returns
+*/
+static int get_cursor(lua_State *L) 
+{
+        clang_object clang_parser = to_clangobject(L, 1);
+        CXCursor* cur = new_CXCursor(L);
+        *cur = clang_getTranslationUnitCursor(clang_parser.tu);
+        if (clang_Cursor_isNull(*cur)) {
+                lua_pushnil(L);
+        }
+        return 1;
+}
+
+static luaL_Reg clang_functions[] = {
+    {"newParser", create_clangobject},
+    {"disposeParser", dispose_clangobject},
+    {"getCursor", get_cursor},
+    {"__gc", dispose_clangobject},
+    {NULL, NULL}
+};
+
 
 /* Create CXType as userdata */
 
@@ -73,111 +116,6 @@ static CXType *new_CXType(lua_State *L)
         lua_setmetatable(L, -2);
         return t;
 }
-
-
-/* --Clang function-- */
-
-/*      
-        Format - createIndex(exclude_pch, diagnostics)
-        Parameters - 1. exclude_pch - When non-zero, allows enumeration of "local" declarations
-                     2. diagnostics - Display diagnostics
-        More info - https://clang.llvm.org/doxygen/group__CINDEX.html#ga51eb9b38c18743bf2d824c6230e61f93
-*/
-static int create_CXIndex(lua_State *L) 
-{
-        int exclude_pch = lua_toboolean(L, 1);
-        int diagnostics = lua_toboolean(L, 2);
-        CXIndex *idx = new_CXIndex(L);
-        *idx = clang_createIndex(exclude_pch, diagnostics);
-        return 1;
-}
-
-static luaL_Reg clang_function[] = {
-        {"createIndex", create_CXIndex},
-        {NULL, NULL}
-};
-
-
-/* --Index functions-- */
-
-/*      
-        Format - idx:disposeIndex()
-        Parameter -  idx - Index to be destroyed       
-        More info - https://clang.llvm.org/doxygen/group__CINDEX.html#ga166ab73b14be73cbdcae14d62dbab22a
-*/
-static int dispose_CXIndex(lua_State *L) 
-{
-        CXIndex idx = to_CXIndex(L, 1);
-        luaL_argcheck(L, idx != NULL, 1, "arg disposed");
-        clang_disposeIndex(idx);
-        idx = NULL;
-        return 0;
-}
-
-/*      
-        Format - idx:parseTU(file_name)
-        Parameters - 1. idx - The index object with which the translation unit will be associated  
-                     2. file_name - The name of the source file to load    
-        More info - https://clang.llvm.org/doxygen/group__CINDEX__TRANSLATION__UNIT.html#ga2baf83f8c3299788234c8bce55e4472e
-*/
-static int parse_TU(lua_State *L)
-{
-        CXIndex idx = to_CXIndex(L, 1);
-        const char *file_name = lua_tostring(L, 2);
-        const char *args[] = {file_name};
-        CXTranslationUnit *tu = new_CXTU(L);
-        *tu = clang_parseTranslationUnit(idx, 0, args, 1, 0, 0, CXTranslationUnit_None);
-        return 1;
-}
-
-
-static luaL_Reg index_functions[] = {
-        {"disposeIndex", dispose_CXIndex},
-        {"parseTU", parse_TU},
-        {"__gc", dispose_CXIndex},
-        {NULL, NULL}
-};
-
-
-/* --Translation unit functions-- */
-
-/*      
-        Format - tu:disposeTU()
-        Parameter - tu - Translation unit to be destroyed       
-        More info - https://clang.llvm.org/doxygen/group__CINDEX__TRANSLATION__UNIT.html#gaee753cb0036ca4ab59e48e3dff5f530a
-*/
-static int dispose_CXTU(lua_State *L) 
-{
-        CXTranslationUnit tu = to_CXTU(L, 1);
-        luaL_argcheck(L, tu != NULL, 1, "arg disposed");
-        clang_disposeTranslationUnit(tu);
-        tu = NULL;
-        return 0;
-}
-
-/*      
-        Format - tu:getCursor()
-        Parameter - tu - Translation unit of which the cursor represents     
-        More info - https://clang.llvm.org/doxygen/group__CINDEX__CURSOR__MANIP.html#gaec6e69127920785e74e4a517423f4391
-*/
-static int get_TU_cursor(lua_State *L) 
-{
-        CXTranslationUnit tu = to_CXTU(L, 1);
-        CXCursor* cur = new_CXCursor(L);
-        *cur = clang_getTranslationUnitCursor(tu);
-        if (clang_Cursor_isNull(*cur)) {
-                lua_pushnil(L);
-        }
-        return 1;
-}
-
-
-static luaL_Reg tu_functions[] = {
-        {"disposeTU", dispose_CXTU},
-        {"getCursor", get_TU_cursor},
-        {"__gc", dispose_CXTU},
-        {NULL, NULL}
-};
 
 
 /* --Cursor functions-- */
@@ -364,12 +302,11 @@ void new_metatable(lua_State *L, const char *name, luaL_Reg *reg)
 
 int luaopen_luaclang(lua_State *L) 
 {
-        new_metatable(L, INDEX_METATABLE, index_functions);
-        new_metatable(L, TU_METATABLE, tu_functions);
+        new_metatable(L, CLANG_METATABLE, clang_functions);
         new_metatable(L, CURSOR_METATABLE, cursor_functions);
 
         lua_newtable(L);
-        luaL_setfuncs(L, clang_function, 0);
+        luaL_setfuncs(L, clang_functions, 0);
 
         return 1;
 }
