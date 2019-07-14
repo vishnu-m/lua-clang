b4b1418f7fb9b0e38938ac7aca5a7c1aea28d67d
Vishnu-M <vishnumurali7000@gmail.com>
2019-07-14 20:16:23 +0530

    Main functions PR merged

--- src/luaclang.c
+++ src/luaclang.c
@@ -1,34 +1,28 @@
 #include <clang-c/Index.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
 
-#define CLANG_METATABLE  "Clang.Parser"
+#define PARSER_METATABLE  "Clang.Parser"
 #define CURSOR_METATABLE "Clang.Cursor"
-#define TYPE_METATABLE   "Clang.Type"
+
+#define new_object(L, ptr, mt) {\
+	ptr = (typeof(ptr)) lua_newuserdata(L, sizeof(*ptr)); \
+	luaL_getmetatable(L, mt); \
+	lua_setmetatable(L, -2); }
+
+#define to_object(L, ptr, mt, n) {\
+        ptr = (typeof(ptr)) luaL_checkudata(L, n, mt); }
 
 typedef struct clang_parser {
         CXIndex idx;
         CXTranslationUnit tu;
 } clang_parser;
 
-/* Create clang_parser as userdata */
-static clang_parser *new_clangparser(lua_State *L) 
-{
-        clang_parser *parser = (clang_parser*) lua_newuserdata(L, sizeof(clang_parser));
-        luaL_getmetatable(L, CLANG_METATABLE);
-        lua_setmetatable(L, -2);
-        return parser;
-}
-
-/* Convert userdata type to  */
-static clang_parser *to_clangparser(lua_State *L, int n) 
-{
-        clang_parser *parser = (clang_parser*) luaL_checkudata(L, n, CLANG_METATABLE);
-        return parser;
-}
+/* --Clang functions-- */
 
 /*      
         Format - newParser(file_name)
@@ -37,13 +31,19 @@ static clang_parser *to_clangparser(lua_State *L, int n)
                     2. https://clang.llvm.org/doxygen/group__CINDEX__TRANSLATION__UNIT.html#ga2baf83f8c3299788234c8bce55e4472e
         Returns clang object whose translation unit cursor can be obtained.
 */
-static int create_clangparser(lua_State *L)
+static int clang_newparser(lua_State *L)
 {
         const char *file_name = lua_tostring(L, 1);
-        clang_parser *parser = new_clangparser(L);
+        if (access(file_name, F_OK) == -1) {
+             return luaL_error(L, "file doesn't exist");    
+        }
+        clang_parser *parser;
+        new_object(L, parser, PARSER_METATABLE);
         parser->idx = clang_createIndex(1, 0);
+        luaL_argcheck(L, parser->idx != NULL, 1, "index wasn't created");
         const char *args[] = {file_name};
         parser->tu = clang_parseTranslationUnit(parser->idx, 0, args, 1, 0, 0, CXTranslationUnit_None);
+        luaL_argcheck(L, parser->tu != NULL, 1, "translation unit wasn't created");
         return 1;
 }
 
@@ -54,9 +54,10 @@ static int create_clangparser(lua_State *L)
                     2. https://clang.llvm.org/doxygen/group__CINDEX__TRANSLATION__UNIT.html#ga2baf83f8c3299788234c8bce55e4472e
         Returns nothing
 */
-static int dispose_clangobject(lua_State *L)
+static int parser_dispose(lua_State *L)
 {
-        clang_parser *parser = to_clangparser(L, 1);
+        clang_parser *parser;
+        to_object(L, parser, PARSER_METATABLE, 1);
         if (parser->idx == NULL) return 0;
         clang_disposeIndex(parser->idx);
         clang_disposeTranslationUnit(parser->tu);
@@ -65,59 +66,25 @@ static int dispose_clangobject(lua_State *L)
         return 0;
 }
 
-/* Create CXCursor as userdata */
-static inline CXCursor *new_CXCursor(lua_State *L) 
-{
-        CXCursor *cur = (CXCursor*) lua_newuserdata(L, sizeof(CXCursor));
-        luaL_getmetatable(L, CURSOR_METATABLE);
-        lua_setmetatable(L, -2);
-        return cur;
-}
-
-/* Convert userdata type to CXCursor */
-static  CXCursor to_CXCursor(lua_State *L, int n) 
-{
-        CXCursor *c = (CXCursor*) luaL_checkudata(L, n, CURSOR_METATABLE);
-        return *c;
-}
-
 /*      
         Format - parser:getCursor()
         Parameter - parser - Clang object whose translation unit cursor is to be obtained 
         More info - https://clang.llvm.org/doxygen/group__CINDEX__CURSOR__MANIP.html#gaec6e69127920785e74e4a517423f4391
         Returns
 */
-static int get_cursor(lua_State *L) 
+static int parser_getcursor(lua_State *L) 
 {
-        clang_parser *parser = to_clangparser(L, 1);
-        CXCursor* cur = new_CXCursor(L);
+        clang_parser *parser;
+        to_object(L, parser, PARSER_METATABLE, 1);
         luaL_argcheck(L, parser->tu != NULL, 1, "parser object was disposed");
+        CXCursor *cur;
+        new_object(L, cur, CURSOR_METATABLE);
         *cur = clang_getTranslationUnitCursor(parser->tu);
         if (clang_Cursor_isNull(*cur)) {
                 lua_pushnil(L);
         }
         return 1;
 }
-
-static luaL_Reg clang_functions[] = {
-    {"newParser", create_clangparser},
-    {"disposeParser", dispose_clangobject},
-    {"getCursor", get_cursor},
-    {"__gc", dispose_clangobject},
-    {NULL, NULL}
-};
-
-
-/* Create CXType as userdata */
-
-static CXType *new_CXType(lua_State *L)
-{
-        CXType *t = (CXType*) lua_newuserdata(L, sizeof(CXType));
-        luaL_getmetatable(L, TYPE_METATABLE);
-        lua_setmetatable(L, -2);
-        return t;
-}
-
 
 /* --Cursor functions-- */
 
@@ -126,26 +93,22 @@ static CXType *new_CXType(lua_State *L)
         Parameter - cur - Cursor whose name is to be obtained    
         More info - https://clang.llvm.org/doxygen/group__CINDEX__CURSOR__XREF.html#gaad1c9b2a1c5ef96cebdbc62f1671c763
 */
-static int get_cursor_spelling(lua_State *L) 
+static int cursor_getspelling(lua_State *L) 
 {
-        CXCursor cur = to_CXCursor(L, 1);
-        CXString name = clang_getCursorSpelling(cur);
+        CXCursor *cur;
+        to_object(L, cur, CURSOR_METATABLE, 1);
+        CXString name = clang_getCursorSpelling(*cur);
         lua_pushstring(L, clang_getCString(name));
         clang_disposeString(name);
         return 1;
 }
 
-/*      
-        Format - cur:getType()
-        Parameter - cur - Cursor whose type is to be obtained    
-        More info - https://clang.llvm.org/doxygen/group__CINDEX__TYPES.html#gaae5702661bb1f2f93038051737de20f4
-*/
-static int get_cursor_type(lua_State *L)
+static int cursor_gettype(lua_State *L)
 {
-        CXCursor cur = to_CXCursor(L, 1);
-        CXType *type = new_CXType(L);
-        *type = clang_getCursorType(cur);
-        CXString type_spelling = clang_getTypeSpelling(*type);
+        CXCursor *cur;
+        to_object(L, cur, CURSOR_METATABLE, 1);;
+        CXType type = clang_getCursorType(*cur);
+        CXString type_spelling = clang_getTypeSpelling(type);
         lua_pushstring(L, clang_getCString(type_spelling));
         clang_disposeString(type_spelling);
         return 1;
@@ -185,10 +148,11 @@ static const char *cursor_kind_str(enum CXCursorKind kind)
         Parameter - cur - Cursor whose kind is to be obtained    
         More info - https://clang.llvm.org/doxygen/group__CINDEX__CURSOR__MANIP.html#ga018aaf60362cb751e517d9f8620d490c
 */
-static int get_cursor_kind(lua_State *L)
+static int cursor_getkind(lua_State *L)
 {
-        CXCursor cur = to_CXCursor(L, 1);
-        lua_pushstring(L, cursor_kind_str(clang_getCursorKind(cur)));
+        CXCursor *cur;
+        to_object(L, cur, CURSOR_METATABLE, 1);
+        lua_pushstring(L, cursor_kind_str(clang_getCursorKind(*cur)));
         return 1;
 }
 
@@ -216,8 +180,9 @@ static const char *storage_class_str(enum CX_StorageClass sc_specifier)
  */
 static int get_storage_class(lua_State *L)
 {
-        CXCursor cur = to_CXCursor(L, 1);
-        enum CX_StorageClass sc_specifier = clang_Cursor_getStorageClass(cur);
+        CXCursor *cur;
+        to_object(L, cur, CURSOR_METATABLE, 1);
+        enum CX_StorageClass sc_specifier = clang_Cursor_getStorageClass(*cur);
         lua_pushstring(L, storage_class_str(sc_specifier));
         return 1;
 }
@@ -229,10 +194,10 @@ static int get_storage_class(lua_State *L)
  */
 static int get_result_type(lua_State *L)
 {
-        CXCursor cur = to_CXCursor(L, 1);
-        CXType *type = new_CXType(L);
-        *type = clang_getCursorType(cur);
-        CXString result_type = clang_getTypeSpelling(clang_getResultType(*type));
+        CXCursor *cur;
+        to_object(L, cur, CURSOR_METATABLE, 1);
+        CXType type = clang_getCursorType(*cur);
+        CXString result_type = clang_getTypeSpelling(clang_getResultType(type));
         lua_pushstring(L, clang_getCString(result_type));
         return 1;
 }
@@ -244,8 +209,9 @@ static int get_result_type(lua_State *L)
 */
 static int get_num_args(lua_State *L)
 {
-        CXCursor cur = to_CXCursor(L, 1);
-        int num_args = clang_Cursor_getNumArguments(cur);
+        CXCursor *cur;
+        to_object(L, cur, CURSOR_METATABLE, 1);
+        int num_args = clang_Cursor_getNumArguments(*cur);
         lua_pushnumber(L, num_args);
         return 1;
 }
@@ -257,9 +223,10 @@ static int get_num_args(lua_State *L)
 */
 static int is_function_inline(lua_State *L)
 {
-        CXCursor cur = to_CXCursor(L, 1);
+        CXCursor *cur;
+        to_object(L, cur, CURSOR_METATABLE, 1);
         CINDEX_LINKAGE bool is_inline;
-        is_inline = clang_Cursor_isFunctionInlined(cur);
+        is_inline = clang_Cursor_isFunctionInlined(*cur);
         lua_pushboolean(L, is_inline);
         return 1;
 }
@@ -271,26 +238,16 @@ static int is_function_inline(lua_State *L)
 */
 static int get_argument(lua_State *L)
 {
-        CXCursor cur = to_CXCursor(L, 1);
+        CXCursor *cur;
+        to_object(L, cur, CURSOR_METATABLE, 1);
         int arg_index = lua_tointeger(L, 2);
-        CXCursor arg_cursor = clang_Cursor_getArgument(cur, arg_index);
+        CXCursor arg_cursor = clang_Cursor_getArgument(*cur, arg_index);
         CXString arg_cursor_name = clang_getCursorSpelling(arg_cursor);
         lua_pushstring(L, clang_getCString(arg_cursor_name));
         clang_disposeString(arg_cursor_name);
         return 1;
 }
 
-static luaL_Reg cursor_functions[] = {
-        {"getSpelling", get_cursor_spelling},
-        {"getType", get_cursor_type},
-        {"getKind", get_cursor_kind},
-        {"getStorageClass", get_storage_class},
-        {"getResultType", get_result_type},
-        {"getNumArgs", get_num_args},
-        {"isFunctionInline", is_function_inline},
-        {"getArgument", get_argument},
-        {NULL, NULL}
-};
 
 void new_metatable(lua_State *L, const char *name, luaL_Reg *reg)
 {
@@ -301,12 +258,35 @@ void new_metatable(lua_State *L, const char *name, luaL_Reg *reg)
         lua_pop(L, 1);
 }
 
+static luaL_Reg clang_functions[] = {
+        {"newParser", clang_newparser},
+        {NULL, NULL}
+};
+
+static luaL_Reg parser_functions[] = {
+        {"dispose", parser_dispose},
+        {"getCursor", parser_getcursor},
+        {"__gc", parser_dispose},
+        {NULL, NULL}
+};
+
+static luaL_Reg cursor_functions[] = {
+        {"getSpelling", cursor_getspelling},
+        {"getType", cursor_gettype},
+        {"getKind", cursor_getkind},
+        {"getStorageClass", get_storage_class},
+        {"getResultType", get_result_type},
+        {"getNumArgs", get_num_args},
+        {"isFunctionInline", is_function_inline},
+        {"getArgument", get_argument},
+        {NULL, NULL}
+};
+
 int luaopen_luaclang(lua_State *L) 
 {
-        new_metatable(L, CLANG_METATABLE, clang_functions);
+        new_metatable(L, PARSER_METATABLE, parser_functions);
         new_metatable(L, CURSOR_METATABLE, cursor_functions);
         lua_newtable(L);
         luaL_setfuncs(L, clang_functions, 0);
         return 1;
 }
-
